1576f530ebac9da58de0a4cea909264c111c1795
wy373226722 <a@12345679.cn>
2016-05-20 22:30:22 +0800

    分离控制器

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ import path from 'path';
 import serve from 'koa-static';
 import marked from 'marked';
 import {CheckPassword} from './app/tool/ass.js';
-import moment from 'moment';
+import index from './api/controllers/index';
 //import assemble from 'assemble';
 
 //初始化
@@ -28,9 +28,6 @@ const app = new Koa();
 const router = new Koa_router();
 const body_parser = new Koa_body_parser();
 
-const env = system_config.System_type || 'development';//判断开发模式
-const mysql_prefix = system_config.mysql_prefix;//数据库前缀
-moment.locale('zh-cn');
 
 app //初始化中间件
     .use(Koa_convert(body_parser))  //获取表单信息中间件
@@ -54,60 +51,7 @@ app //初始化中间件
     }));
 
 router
-    .get('/', (ctx) => {
-        
-        if(env == "production"){
-            //预留
-        }
-
-        var sql = {
-            options: "SELECT `option_name`,`option_value` FROM `bm_options` WHERE `option_id` < 7",
-            post: "SELECT `bm_posts`.`ID` ,`post_title`,`post_date`, `post_content`,`display_name` FROM `" + mysql_prefix + "posts`,`" + mysql_prefix + "users` WHERE `post_type` = 'post' AND `post_status` = 'publish' AND `post_author` = `bm_users`.`ID` ORDER BY `bm_posts`.`ID` DESC LIMIT 10",
-            post_all: "SELECT count(`bm_posts`.`ID`) AS `posts_all` FROM `bm_posts`,`bm_users` WHERE `post_type` = 'post' AND `post_status` = 'publish' AND `post_author` = `bm_users`.`ID`",
-            tag: "SELECT `name` AS `tag_name` FROM `bm_terms` WHERE `term_id` IN ( SELECT * FROM (SELECT `term_id` FROM `bm_term_taxonomy` WHERE `taxonomy` = 'post_tag' ORDER BY `count` DESC LIMIT 15) AS `term_id`)",
-            category: "SELECT `name` AS `category_name` FROM `bm_terms` WHERE `term_id` in (SELECT `term_id` FROM `bm_term_taxonomy` WHERE `taxonomy` = 'category' AND `count` != 0)",
-            friendly_link: "SELECT `link_url`,`link_name`,`link_target` FROM `bm_links` WHERE `link_id` in (SELECT `object_id` FROM `bm_term_relationships` WHERE `term_taxonomy_id` in (SELECT `term_id`  FROM `bm_terms` WHERE `name` = '友情链接'))"
-        };
-
-        return querys(sql).then((result) => {
-            if (result.length == 0) {
-                ctx.throw(404, '未找该页面或没有任何文章内容!');
-            } else {
-                result.post[0].post_date = moment(result.post[0].post_date).format('ll');
-                for (var a = 0; a < result.post.length; a++) {
-                    result.post[a].post_content = setString(result.post[a].post_content.replace(/<[^>]+>/g, ""), 200);//去掉所有的html标记
-                }
-
-                var options = "{";
-                for (var n = 0; n < result.options.length; n++) {
-                    options = options + "\"" + result.options[n].option_name + "\":\"" + result.options[n].option_value + "\"";
-                    if (n < result.options.length - 1) {
-                        options = options + ",";
-                    }
-                }
-                options = JSON.parse(options + "}");
-
-
-                var temp = parseInt(parseInt(result.post_all[0].posts_all) / 10 + 1);
-                var posts_all = [temp];
-                for (var z = 0; z < temp; z++) {
-                    posts_all[z] = z + 1;
-                }
-
-                var posts = {
-                    options: options,
-                    posts: result.post,
-                    posts_all: posts_all,
-                    posts_now: "1",
-                    post_tag: result.tag,
-                    post_category: result.category,
-                    friendly_link: result.friendly_link
-                };
-
-                ctx.render('list', posts);
-            }
-        });
-    })
+    .get('/', index)
 
     .get('/page/:num', (ctx) => {
         var limit = parseInt((parseInt(ctx.params.num) - 1) * 10) + "," + 10;
