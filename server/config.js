774ce432273b6b1e49723c0df7c225eb91130e76
wy373226722 <a@12345679.cn>
2016-05-21 23:52:07 +0800

    add test数据库

--- server/config.js
+++ server/config.js
@@ -11,7 +11,7 @@ export var system_config = {
     mysql_host: 'localhost', //MySQL服务器地址
     mysql_user: 'root', //数据库用户名
     mysql_password: 'root', //数据库密码
-    mysql_database: 'bmblog', //数据库名称
+    mysql_database: 'test', //数据库名称
     mysql_port: 3306, //数据库端口号
     mysql_prefix: 'bm_' //默认"bm_"
 };
