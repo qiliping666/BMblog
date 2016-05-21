8d94698abac5f37e20a8342907e351b400902e14
wy373226722 <a@12345679.cn>
2016-05-22 01:04:24 +0800

    add home changes

--- server/api/controllers/home.js
+++ server/api/controllers/home.js
@@ -54,8 +54,8 @@ export var list = (ctx) => {
         if (result.length == 0) {
             ctx.throw(404, '未找该页面或没有任何文章内容!');
         } else {
-            for (var a = 0; a < result.post.length; a++) {
-                result.post[a].post_content = setString(result.post[a].post_content.replace(/<[^>]+>/g, ""), 200);//去掉所有的html标记
+            for (var a = 0; a < result.posts.length; a++) {
+                result.posts[a].post_content = setString(result.posts[a].post_content.replace(/<[^>]+>/g, ""), 200);//去掉所有的html标记
             }
 
             var options = "{";
@@ -68,7 +68,7 @@ export var list = (ctx) => {
             options = JSON.parse(options + "}");
 
 
-            var temp = parseInt(parseInt(result.post_all[0].posts_all) / 10 + 1);
+            var temp = parseInt(parseInt(result.posts_all[0].posts_all) / 10 + 1);
             var posts_all = [temp];
             for (var z = 0; z < temp; z++) {
                 posts_all[z] = z + 1;
@@ -76,7 +76,7 @@ export var list = (ctx) => {
 
             var posts = {
                 options: options,
-                posts: result.post,
+                posts: result.posts,
                 posts_all: posts_all,
                 posts_now: ctx.params.num,
                 post_tag: result.tag,
--- server/api/models/home.js
+++ server/api/models/home.js
@@ -43,12 +43,13 @@ export var list = (ctx)=> {
    var limit = parseInt((parseInt(ctx.params.num) - 1) * 10) + "," + 10;
    return Promise.all([
         pool.query(find_sql),
-        pool.query("SELECT `bm_posts`.`ID` ,`post_title`,`post_date`, `post_content`,`display_name` FROM `" + mysql_prefix + "posts`,`" + mysql_prefix + "users` WHERE `post_type` = 'post' AND `post_status` = 'publish' AND `post_author` = `bm_users`.`ID` ORDER BY `bm_posts`.`ID` DESC LIMIT " + limit,),
+        pool.query("SELECT `bm_posts`.`ID` ,`post_title`,`post_date`, `post_content`,`display_name` FROM `" + mysql_prefix + "posts`,`" + mysql_prefix + "users` WHERE `post_type` = 'post' AND `post_status` = 'publish' AND `post_author` = `bm_users`.`ID` ORDER BY `bm_posts`.`ID` DESC LIMIT " + limit),
         pool.query("SELECT count(`bm_posts`.`ID`) AS `posts_all` FROM `bm_posts`,`bm_users` WHERE `post_type` = 'post' AND `post_status` = 'publish' AND `post_author` = `bm_users`.`ID`"),
         pool.query("SELECT `name` AS `tag_name` FROM `bm_terms` WHERE `term_id` IN ( SELECT * FROM (SELECT `term_id` FROM `bm_term_taxonomy` WHERE `taxonomy` = 'post_tag' ORDER BY `count` DESC LIMIT 15) AS `term_id`)"),
         pool.query("SELECT `name` AS `category_name` FROM `bm_terms` WHERE `term_id` in (SELECT `term_id` FROM `bm_term_taxonomy` WHERE `taxonomy` = 'category' AND `count` != 0)"),
         pool.query("SELECT `link_url`,`link_name`,`link_target` FROM `bm_links` WHERE `link_id` in (SELECT `object_id` FROM `bm_term_relationships` WHERE `term_taxonomy_id` in (SELECT `term_id`  FROM `bm_terms` WHERE `name` = '友情链接'))")
     ]).then(data => {
+       console.log(data);
         return {
             options: data[0],
             posts: data[1],
